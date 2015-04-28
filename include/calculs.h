b601013be9ea3c9da420d81789d42ad5dd173099
Laurent <laurent@bassin.info>
2015-04-28 11:10:15 +0200

    Version Finale

--- include/calculs.h
+++ include/calculs.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 	
 	void deplacementPerso(Perso* persoHaut, Perso* persoBas, int persoActuel, int direction);
 	int collisionSolY(Perso* perso, Sol* plateforme);
@@ -8,12 +7,3 @@
 	void collisionPiege(Perso* perso, Piege* piege);
 	void scroll(Perso* perso, Sol* plateforme, Piece* piece, Piege* piege, int ecran);
 	int fin(Perso* persoHaut, Perso* persoBas, int persoActuel, int* bg, int* bgSub,  int* nbPiece,  Sol* solHaut, Sol* solBas);
-=======
-	
-	void deplacementPerso(Perso* persoHaut, Perso* persoBas, int persoActuel, int direction);
-	int collisionSolY(Perso* perso, Sol* plateforme);
-	int collisionSolX(Perso* perso, Sol* plateforme);
-	void collisionPiece(Perso* perso, Piece* piece);
-	void collisionPiege(Perso* perso, Piege* piege);
-	void scroll(Perso* perso, Sol* plateforme, Piece* piece, Piege* piege, int ecran);
->>>>>>> 09bf57a2209d2f8718808e1f34adc55117b2738a
--- include/variables.h
+++ include/variables.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <nds.h>
 #include <stdio.h>
 
@@ -96,73 +95,4 @@
 	
 	enum {STOP, DROITE, GAUCHE, SAUT};
 	enum {X,Y};
-=======
-#include <nds.h>
-#include <stdio.h>
-
-#define NBPLATEFORME 3
-#define NBPIECEHAUT 2
-#define NBPIEGEBAS 2
-
-	typedef struct Perso Perso;
-	struct Perso{
-		float x;
-		float y;
-		
-		float vitesse;
-		float hauteurSaut;
-		
-		float hauteur;
-		float largeur;
-		
-		float deplacement[2];
-		float acceleration[2];
-		
-		int nbSaut;
-		
-		int anim;
-		
-		int imgActuelle;
-		u16* imgSprite[16];
-	};
-	
-	typedef struct Sol Sol;
-	struct	Sol{
-		float x;
-		float y;
-		
-		int hauteur;
-		int longueur;
-		u16* imgSol;
-	};
-	
-	typedef struct Piece Piece;
-	struct Piece{
-		float x;
-		float y;
-		
-		int tailleCote;
-		
-		int visible;
-		
-		int imgActuelle;
-		u16* img[6];
-	};
-	
-	typedef struct Piege Piege;
-	struct Piege{
-		float x;
-		float y;
-		
-		int tailleCote;
-		
-		int animation;
-		
-		int imgActuelle;
-		u16* img[9];
-	};
-		
-	enum {STOP, DROITE, GAUCHE, SAUT};
-	enum {X,Y};
->>>>>>> 09bf57a2209d2f8718808e1f34adc55117b2738a
 	enum {HAUT, BAS};
